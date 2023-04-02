f9e703dc6a120d92f2b44836f9f680c7b746ce3a
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-04-02 18:32:10 +0300

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { filtersReducer } from './filtersSlice';
 import { tasksReducer } from './tasksSlice';
 
-export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filtersReducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['filters'],
+};
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
 });
+
+const persistedTasksReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedTasksReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
